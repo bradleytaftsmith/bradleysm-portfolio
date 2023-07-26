a940691d61f749dfa949ef4669043fdef9d3596b
Bradley Smith <bradleysmith@Bradleys-MacBook-Pro.local>
2023-07-26 12:38:41 -0500

    my fork of Hosna Qasnei's portfolio app

---
+++ README.md
@@ -0,0 +1,24 @@
+# Tailwind CSS and Next.js Portfolio
+
+This is a template for creating a portfolio with Tailwind CSS and Next.js.
+
+![Portfolio Demo](demo/demo.gif)
+
+## How it works
+
+Fork or download the repo and change whatever you need to change for your needs.
+
+## Running Locally
+
+Can run the application in VS Code or a terminal and it will be available at `http://localhost:3000`.
+
+```bash
+npm install
+npm run dev
+```
+
+## Attribution
+
+Thanks to [Hosna Qasmei](https://hosnaqasmei.com/) for her [portfolio tutorial](https://www.youtube.com/watch?v=7gnVzj_nMJw) and [template](https://github.com/hqasmei/tailwindcss-and-nextjs-portfolio). I did her tutorial for the heck of it and changed the template to my own content.
+
+Illustrations and avatars were generated using [Midjourney](https://www.midjourney.com/).
---
+++ components/AboutSection.tsx
@@ -0,0 +1,89 @@
+import React from "react"
+import Image from "next/image"
+
+const skills = [
+  { skill: "product strategy" },
+  { skill: "KPIs & goal alignment" },
+  { skill: "product-led growth" },
+  { skill: "sales-led growth" },
+  { skill: "product validation" },
+  { skill: "user research" },
+  { skill: "Figma" },
+  { skill: "GIS" },
+  { skill: "Postgresql" }
+]
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <p>
+              I'm 
+              {" "}
+              <span className="font-bold text-green-500">
+                Bradley Smith
+              </span>{""}
+              , a 
+              <span className="font-bold">{" playful"}</span> and
+              <span className="font-bold">{" capable"}</span> product manager and designer based in Austin, TX. 
+            </p>
+            <br />
+            <p>I help lead software products with 
+            {" "}
+              <span className="font-bold text-green-500">
+              user-centered 
+              </span>{" "}
+                design and development methods. I have experience in B2B SaaS products in a variety of industries, and am always hungry to learn about new ones.
+            </p>
+            <br />
+            <p>
+              I believe that {" "}
+              <span className="font-bold text-green-500">
+                embracing failure
+              </span>{" "}
+            leads to growth, learning, and all around better outcomes. It takes many failures to make a success.
+            </p>
+            <br />
+            <p>
+              I {" "}
+              <span className="font-bold text-green-500">
+              play a lot of skeeball
+              </span>{""}, like to build things with my hands or with automated tools, and appreciate every opportunity I get to hang out with my family.
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            <Image
+              src="/hero-image.png"
+              alt=""
+              width={325}
+              height={325}
+              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
